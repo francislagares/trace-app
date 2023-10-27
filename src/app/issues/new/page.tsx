cd597c6749a1916da2738149b128d36c42cfeb7d
Francis Lagares <francis.lagares@gmail.com>
2023-10-27 20:54:37 +0200

    feat: create ErrorMessage component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -49,11 +49,11 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
-        {errors.title && (
-          <Text color='red' as='p'>
-            {errors.title.message}
-          </Text>
-        )}
+
+        <Text color='red' as='p'>
+          {errors.title?.message}
+        </Text>
+
         <Controller
           name='description'
           control={control}
@@ -61,11 +61,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder='Description' {...field} />
           )}
         />
-        {errors.description && (
-          <Text color='red' as='p'>
-            {errors.description.message}
-          </Text>
-        )}
+
+        <Text color='red' as='p'>
+          {errors.description?.message}
+        </Text>
+
         <Button>Submit New Issue</Button>
       </form>
     </div>
