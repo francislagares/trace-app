25a88b56a2b9679e834753a714f93df1efe2db98
Francis Lagares <francis.lagares@gmail.com>
2023-10-29 18:29:45 +0100

    feat: render list of issues

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -8,23 +8,36 @@ const IssuesPage = async () => {
 
   return (
     <div>
-      <Button>
-        <Link href='/issues/new'>New Issue</Link>
-      </Button>
+      <div className='mb-5'>
+        <Button>
+          <Link href='/issues/new'>New Issue</Link>
+        </Button>
+      </div>
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {issues.map(issue => (
             <Table.Row key={issue.id}>
-              <Table.Cell>{issue.title}</Table.Cell>
-              <Table.Cell>{issue.status}</Table.Cell>
-              <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell>
+                {issue.title}
+                <div className='block md:hidden'>{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
