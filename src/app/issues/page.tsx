d98d06fc2205472cb4214f1e181ce36c52eee3a6
Francis Lagares <francis.lagares@gmail.com>
2023-10-29 20:35:33 +0100

    feat: create issue details page

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,5 +1,6 @@
 import { Table } from '@radix-ui/themes';
 import delay from 'delay';
+import Link from 'next/link';
 
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 
@@ -30,7 +31,7 @@ const IssuesPage = async () => {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className='block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
