ba94bef4923e4a6ee476b800c6e12f88e271ecfe
Francis Lagares <francis.lagares@gmail.com>
2023-11-01 20:49:58 +0100

    feat: add edit issue button

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 
@@ -20,16 +22,24 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex className='space-x-3' my='2'>
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose' mt='4'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex className='space-x-3' my='2'>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose' mt='4'>
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
