51ac164e2ad5fd139a4915d6479971a83d3c8256
Francis Lagares <francis.lagares@gmail.com>
2023-11-02 17:04:37 +0100

    feat: create edit issue page

---
+++ src/app/issues/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import { notFound } from 'next/navigation';
+
+import IssueForm from '@/app/issues/_components/IssueForm';
+
+import prisma from 'prisma/client';
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
---
+++ src/app/issues/_components/IssueForm.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { useState } from 'react';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Issue } from '@prisma/client';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
+
+import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/schemas/validationSchemas';
+
+import 'easymde/dist/easymde.min.css';
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submitHandler = handleSubmit(async data => {
+    try {
+      setIsSubmitting(isSubmitting => !isSubmitting);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setError('An unexpected error occurred.');
+    }
+  });
+
+  return (
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className='space-y-3' onSubmit={submitHandler}>
+        <TextField.Root>
+          <TextField.Input
+            defaultValue={issue?.title}
+            placeholder='Title'
+            {...register('title')}
+          />
+        </TextField.Root>
+
+        <Text color='red' as='p'>
+          {errors.title?.message}
+        </Text>
+
+        <Controller
+          name='description'
+          control={control}
+          defaultValue={issue?.description}
+          render={({ field }) => (
+            <SimpleMDE placeholder='Description' {...field} />
+          )}
+        />
+
+        <Text color='red' as='p'>
+          {errors.description?.message}
+        </Text>
+
+        <Button disabled={isSubmitting}>
+          Submit New Issue
+          {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
