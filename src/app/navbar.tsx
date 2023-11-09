dc46f71e10d4574ee244a1eb48eb45a51b437803
Francis Lagares <francis.lagares@gmail.com>
2023-11-09 19:00:31 +0100

    feat: add dropdown menu to show user

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Box, Container, Flex } from '@radix-ui/themes';
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from '@radix-ui/themes';
 import classNames from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -9,7 +16,7 @@ import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
   const currentPath = usePathname();
-  const { status } = useSession();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -43,7 +50,25 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === 'authenticated' && (
-              <Link href='/api/auth/signout'>Log out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback='?'
+                    size='2'
+                    radius='full'
+                    className='cursor-pointer'
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size='2'>{session.user?.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href='/api/auth/signout'>Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === 'unauthenticated' && (
               <Link href='/api/auth/signin'>Log in</Link>
