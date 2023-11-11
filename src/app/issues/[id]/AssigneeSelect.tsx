b1ab3354244330740a60c5db8aa24267ba6bf5cd
Francis Lagares <francis.lagares@gmail.com>
2023-11-11 14:53:40 +0100

    feat: populate AssigneeSelect component

--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,36 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
+import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import axios from 'axios';
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get<User[]>('/api/users');
+        setUsers(data);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder='Assign...' />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value='1'>Francis Lagares</Select.Item>
+          {users.map(user => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
