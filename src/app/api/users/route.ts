fe7ba3e37b2899637af415b2df35e331f97414df
Francis Lagares <francis.lagares@gmail.com>
2023-11-11 18:53:41 +0100

    feat: fetch users with react-query

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from 'next/server';
 
 import prisma from 'prisma/client';
--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -1,24 +1,27 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+import { Skeleton } from '@/app/components';
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await axios.get<User[]>('/api/users');
-        setUsers(data);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, []);
+const AssigneeSelect = () => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+
+  if (isLoading) return <Skeleton />;
+
+  if (error) return null;
 
   return (
     <Select.Root>
@@ -26,7 +29,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map(user => (
+          {users?.map(user => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
