7242b5c837120d9c60660ef79da4eb28706be707
Francis Lagares <francis.lagares@gmail.com>
2023-11-12 20:32:05 +0100

    refactor: extract inline functions

--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -8,33 +8,33 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import { Skeleton } from '@/app/components';
 
-const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
+const useUsers = () =>
+  useQuery<User[]>({
     queryKey: ['users'],
     queryFn: () => axios.get('/api/users').then(res => res.data),
     staleTime: 60 * 1000,
     retry: 3,
   });
 
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const { data: users, error, isLoading } = useUsers();
+
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
+
+  const assignIssue = (userId: string) =>
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId || null,
+      })
+      .catch(() => toast.error('Changes could not be saved.'));
 
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ''}
-        onValueChange={userId =>
-          axios
-            .patch(`/api/issues/${issue.id}`, {
-              assignedToUserId: userId || null,
-            })
-            .catch(() => toast.error('Changes could not be saved.'))
-        }
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
