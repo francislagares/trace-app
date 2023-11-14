9bff49494e7045ee589708ec7e12b14472391e8a
Francis Lagares <francis.lagares@gmail.com>
2023-11-14 13:56:58 +0100

    feat: filter issues

--- src/app/issues/list/IssueStatusFilter.tsx
+++ src/app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
 
 const statuses: { label: string; value?: Status }[] = [
   { label: 'All' },
@@ -11,12 +12,22 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={status => {
+        const query = status ? `?status=${status}` : '';
+
+        router.push(`/issues/list/${query}`);
+      }}
+    >
       <Select.Trigger placeholder='Filter by status...' />
       <Select.Content>
         {statuses.map(status => (
-          <Select.Item key={status.value} value={status.value || ''} />
+          <Select.Item key={status.value} value={status.value || ''}>
+            {status.label}
+          </Select.Item>
         ))}
       </Select.Content>
     </Select.Root>
--- src/app/issues/list/page.tsx
+++ src/app/issues/list/page.tsx
@@ -1,3 +1,4 @@
+import { Status } from '@prisma/client';
 import { Table } from '@radix-ui/themes';
 
 import { IssueStatusBadge, Link } from '@/app/components';
@@ -5,8 +6,21 @@ import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
 import prisma from 'prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
