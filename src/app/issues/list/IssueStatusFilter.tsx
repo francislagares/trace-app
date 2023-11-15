7ed65bd38b26bc83745bf4a523d441046315ea7c
Francis Lagares <francis.lagares@gmail.com>
2023-11-15 21:02:22 +0100

    fix: url params filter string bug

--- src/app/issues/list/IssueStatusFilter.tsx
+++ src/app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const statuses: { label: string; value?: Status }[] = [
   { label: 'All' },
@@ -13,11 +13,23 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams?.get('status') || ''}
       onValueChange={status => {
-        const query = status ? `?status=${status}` : '';
+        const params = new URLSearchParams();
+
+        if (status) {
+          params.append('status', status);
+        }
+
+        if (searchParams?.get('orderBy')) {
+          params.append('orderBy', searchParams.get('orderBy')!);
+        }
+
+        const query = params.size ? '?' + params.toString() : '';
 
         router.push(`/issues/list/${query}`);
       }}
