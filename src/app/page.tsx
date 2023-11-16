a7ad48e99724993e1e9087bc389f61c018d1088e
Francis Lagares <francis.lagares@gmail.com>
2023-11-16 19:37:36 +0100

    feat: create Pagination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from './components/Pagination';
+
 export default function Home() {
-  return <div>Hello Trace-App</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
