c91e207e30f87497c086d2203c5f37c3bc0a29e7
Francis Lagares <francis.lagares@gmail.com>
2023-11-19 19:44:45 +0100

    feat: create LatestIssues component

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -6,7 +6,5 @@ import Home from '@/app/page';
 vi.mock('next/router', () => require('./mockRouter')); // Mock the useRouter implementation
 
 test('Pages Router', () => {
-  const searchParams = { page: '1' }; // Set up searchParams as needed
-
-  render(<Home searchParams={searchParams} />);
+  render(<Home />);
 });
